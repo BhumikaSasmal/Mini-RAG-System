b4de522d115812c32531709b88a430e9da871e72
BhumikaSasmal <60741385+BhumikaSasmal@users.noreply.github.com>
2026-06-19 16:11:42 +0530

    Update embedding_service.py

--- src/embedding_service.py
+++ src/embedding_service.py
@@ -3,9 +3,21 @@ from sentence_transformers import SentenceTransformer
 from src.config import EMBEDDING_MODEL_NAME
 
 
+_model_instance = None
+
+
+def get_embedding_model(model_name=EMBEDDING_MODEL_NAME):
+    global _model_instance
+
+    if _model_instance is None:
+        _model_instance = SentenceTransformer(model_name)
+
+    return _model_instance
+
+
 class EmbeddingService:
     def __init__(self, model_name=EMBEDDING_MODEL_NAME):
-        self.model = SentenceTransformer(model_name)
+        self.model = get_embedding_model(model_name)
 
     def embed_text(self, text):
         embedding = self.model.encode(text)
